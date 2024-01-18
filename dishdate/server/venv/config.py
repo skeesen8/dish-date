f2a1216e0247cfc6eb4238ae947130cc1b25cc84
Sackett Keesen <skeesen8@gmail.com>
2024-01-18 10:37:43 -0600

    no errors on backend. good to start coding

--- dishdate/server/venv/config.py
+++ dishdate/server/venv/config.py
@@ -1,10 +1,10 @@
 from flask import Flask
-# from flask_cors import CORS
-# from flask_migrate import Migrate
-# from flask_restful import Api
-# from flask_sqlalchemy import SQLAlchemy
-# from sqlalchemy import MetaData
-# from flask_bcrypt import Bcrypt
+from flask_cors import CORS
+from flask_migrate import Migrate
+from flask_restful import Api
+from flask_sqlalchemy import SQLAlchemy
+from sqlalchemy import MetaData
+from flask_bcrypt import Bcrypt
 
 
 
